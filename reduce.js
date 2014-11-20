461602a6e4c472d9f0d1e55ffd8d474da7fb194b
Keith <keithkreissl@gmail.com>
2014-11-19 20:47:20 -0600

    actually got the reduce.js to work

--- reduce.js
+++ reduce.js
@@ -1,8 +1,21 @@
     function countWords(inputWords) {
 	return inputWords.reduce(function(prev, current){
+		if (isNaN(prev[current])){
+			prev[current] = 1
+		} else {
+			prev[current] = prev[current] + 1
+		}
 		return prev
-	})
+	}, {})
       // SOLUTION GOES HERE
     }
 
     module.exports = countWords
+/*  REAL SOLUTION I didn't know about the || to initialize
+      function countWords(arr) {
+        return arr.reduce(function(countMap, word) {
+          countMap[word] = ++countMap[word] || 1 // increment or initialize to 1
+          return countMap
+        }, {}) // second argument to reduce initialises countMap to {}
+      }
+*/
